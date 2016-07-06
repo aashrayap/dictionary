297320f8d2751fd87144a1a8be40509de9c46d7d
aashraya <aashraya_p@hotmail.com>
2016-07-06 15:32:25 -0500

    removed ui

--- dictionary.rb
+++ dictionary.rb
@@ -1,2 +1,22 @@
 class Dictionary
+  def initialize
+    @loader=DictionaryLoader.new
+    @ui=DictionaryUI.new
+    @dictionary
+  end
+  
+  def main
+    @ui.prompt
+  end
+
+  def prompt
+    while true
+    puts "Where is your dictionary?"
+    path=gets.chomp
+    if File.exists?(path)
+      @dictionary=@loader.openfile(path,"r+")
+    else
+      "Invlalid path, try again!"
+    end
+  end
 end
--- loader.rb
+++ loader.rb
@@ -1,7 +1,10 @@
 class DictionaryLoader
+  def intitialize
+  	@file
+  end
+  attr_accessor :file
+
   def openfile(filename,mode)
-  file=File.open(filename,mode)
-  puts "Dictionary successfully loaded"
-  file
+  @file=File.open(filename,mode)
   end
 end
